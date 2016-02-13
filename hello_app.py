0d395c155ec7bc9765829bc36cc4e73588810e86
kittycatdotm <charris59@outlook.com>
2016-02-12 17:45:31 -0800

    Create hello_app.py

---
+++ hello_app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+import os
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+if __name__ == "__main__":
+    port = os.envirn.get("PORT")
+    app.run("0.0.0.0", port)
